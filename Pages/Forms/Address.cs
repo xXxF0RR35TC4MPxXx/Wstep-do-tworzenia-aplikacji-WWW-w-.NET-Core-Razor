27524d2c0750844c2b997fd2051ea53f0927b52a
xXxF0RR35TC4MPxXx <matix2733@gmail.com>
2021-03-14 22:44:43 +0100

    Dodaj pliki projektów.

---
+++ Pages/Forms/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Pages.Forms
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(30, MinimumLength = 2), Required(ErrorMessage ="Pole jest obowiązkowe!")]
+        public string Street { get; set; }
+
+        [StringLength(10, MinimumLength = 2), Required(ErrorMessage = "Podaj numer domu!")]
+        public string Number { get; set; }
+
+        [StringLength(10, MinimumLength = 4), Required(ErrorMessage = "Podaj kod pocztowy!")]
+        public string ZipCode { get; set; }
+
+        [StringLength(30, MinimumLength = 2), Required(ErrorMessage = "Nazwa miasta jest obowiązkowa!")]
+        public string City { get; set; }
+
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using AddressBook.Pages.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        [BindProperty]
+        public Address Address { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            { Name = "User"; }
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+    }
+}
